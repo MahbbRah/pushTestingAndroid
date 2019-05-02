47e5ac129eddd9b34cff9513c41cc7ecc10c478d
Mahbub Rahman <mahbub.meh@gmail.com>
2019-05-02 14:06:48 +0600

    adding everything in package

--- android/app/src/main/java/com/pushtestingapp/MainApplication.java
+++ android/app/src/main/java/com/pushtestingapp/MainApplication.java
@@ -3,6 +3,11 @@ package com.pushtestingapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import io.invertase.firebase.RNFirebasePackage;
+// import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +27,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new AsyncStoragePackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseMessagingPackage()
+          // new RNFirebaseNotificationsPackage()
+          
       );
     }
 
--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+import BgMessaging from './BgMessaging';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => BgMessaging);
